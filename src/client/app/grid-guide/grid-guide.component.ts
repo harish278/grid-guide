3633d408be79d994d42200b1f27f56933ba8a4f2
Harish S <harish@simpragma.com>
2017-07-05 15:47:43 +0530

    Working UI with Images and like functionality

--- src/client/app/grid-guide/grid-guide.component.ts
+++ src/client/app/grid-guide/grid-guide.component.ts
@@ -14,7 +14,14 @@ export class GridGuideComponent implements OnInit {
 
     channelsData: any;
     errorMessage: string;
-    timeArray: any[] = ['7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM', '9:00 PM']
+    // timeArray: any[] = ['0:30 AM', '1:00 AM', '1:30 AM', '2:00 AM', '2:30 AM', '3:00 AM', '3:30 AM',
+    //                     '4:00 AM', '4:30 AM', '5:00 AM', '5:30 AM', '6:00 AM', '6:30 AM', '7:00 AM',
+    //                     '7:30 AM', '8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10: 00 AM', '10:30 AM',
+    //                     '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM',
+    //                     '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM',
+    //                     '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM', '9:00 PM',
+    //                     '9:30 PM', '10:00 PM', '10:30 PM', '11: 00 PM', '11:30 PM', '0:00 AM']
+    timeArray: any[] = ['7:00 PM', '7:30 PM', '8:00 PM', '8:30 PM', '9:00 PM', '9:30 PM', '10:00 PM', '10:30 PM'];
 
     /**
      * Creates an instance of the GridGuideComponent with the injected
@@ -28,13 +35,13 @@ export class GridGuideComponent implements OnInit {
      * Get the names OnInit
      */
     ngOnInit() {
-        this.getNames();
+        this.getChannelsData();
     }
 
     /**
      * Handle the APICallerService observable
      */
-    getNames() {
+    getChannelsData() {
         this.apiCaller.getChannelsData()
             .subscribe(
                 channelsData => {
@@ -42,6 +49,10 @@ export class GridGuideComponent implements OnInit {
                     this.channelsData = channelsData.channels;
                 }, error => this.errorMessage = <any>error
             );
+    }
+
+    toggleLiked(channelIndex: number) {
+        this.channelsData[channelIndex].is_liked = !this.channelsData[channelIndex].is_liked;
     }
 
 }
--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -22,11 +22,10 @@ export class ProjectConfig extends SeedConfig {
     // Add `NPM` third-party libraries to be injected/bundled.
     this.NPM_DEPENDENCIES = [
       ...this.NPM_DEPENDENCIES,
-      // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
-      // {src: 'lodash/lodash.min.js', inject: 'libs'},
-      {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
-      {src: 'bootstrap/dist/js/bootstrap.min.js', inject: 'libs'},
       {src: 'bootstrap/dist/css/bootstrap.min.css', inject: true},
+      {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      // {src: 'lodash/lodash.min.js', inject: 'libs'},
+      {src: 'bootstrap/dist/js/bootstrap.min.js', inject: 'libs'},
     ];
 
     // Add `local` third-party libraries to be injected/bundled.
